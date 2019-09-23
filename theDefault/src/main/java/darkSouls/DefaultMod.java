f7b0e896fc95c485547180968df033289b02476e
GniGamer <ferdinand.rohlfing@googlemail.com>
2019-09-23 17:07:38 +0200

    Updated pom.xml

--- theDefault/src/main/java/darkSouls/DefaultMod.java
+++ theDefault/src/main/java/darkSouls/DefaultMod.java
@@ -86,7 +86,7 @@ public class DefaultMod implements
     public static boolean enablePlaceholder = true; // The boolean we'll be setting on/off (true/false)
 
     //This is for the in-game mod settings panel.
-    private static final String MODNAME = "Default Mod";
+    private static final String MODNAME = "Dark Souls";
     private static final String AUTHOR = "Gremious"; // And pretty soon - You!
     private static final String DESCRIPTION = "A base for Slay the Spire to start your own mod from, feat. the Default.";
     
